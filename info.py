3529ae423110ec9776dbeb3389aa3da8e9def3c1
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-05-29 07:10:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,14 +4,14 @@ from os import environ
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = 1733305
+API_HASH = 'f423cffca6b5b7247b31b5b0df61f48d'
+BOT_TOKEN = '1838208004:AAFckP950IP9aoaIjYG66JnEfHRMtjkW_hw'
 
 # Bot settings
-MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+MAX_RESULTS = 10
+CACHE_TIME = 300
+USE_CAPTION_FILTER =  False
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in environ['ADMINS'].split()]
@@ -20,8 +20,8 @@ auth_users = [int(user) if re.search('^\d+$', user) else user for user in enviro
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = 'mongodb+srv://kaj:kaj@cluster0.xyylh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+DATABASE_NAME = 'kaj'
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
